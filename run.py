5fc7173a1752399677455e4ef9e65fab2b20f2d3
Brian Ledger <brian.peter.ledger@gmail.com>
2026-02-20 15:11:58 -0500

    Add a tornado web-service.

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ import socketserver
 PORT = 8080
 
 
+
+
 def launch_simple_http_server(port=PORT):
   Handler = http.server.SimpleHTTPRequestHandler
 
@@ -15,6 +17,37 @@ def launch_simple_http_server(port=PORT):
       # Start the server and keep it running until you stop the script
       httpd.serve_forever()
 
+
+
+def launch_tornado_http_server(port=PORT):
+  # python3 -m pip install tornado
+  import tornado.ioloop
+  import tornado.web
+
+  class UserHandler(tornado.web.RequestHandler):
+      def get(self, username):
+          # The 'username' argument comes from the regex capture group in the route
+          self.write(f"Hello, user: {username}")
+
+  ### A routes data structure is essential for dynamic services.
+  routes = [
+    (r"/user/([^/]+)", UserHandler),
+    (r"/(.*)", tornado.web.StaticFileHandler, {"path": "concrete"})
+  ]
+
+  def make_app():
+      return tornado.web.Application(routes)
+
+
+  # 3. Start the server
+  if __name__ == "__main__":
+      app = make_app()
+      app.listen(port)
+      print(f"Server is running on http://localhost:{port}")
+      print(f"Test dynamic route: http://localhost:{port}/user/TornadoUser")
+      tornado.ioloop.IOLoop.current().start()
+
 if __name__ == "__main__":
-  launch_simple_http_server()
+  # launch_simple_http_server()
+  launch_tornado_http_server()
   print("Goodbye.")
